f46d1a7469cf912794d120de5e099ca2470cd3f3
keomony.khun <keomony.khun@ktech.com>
2023-09-19 08:23:37 +0100

    Use env variable instead of hardcoded api url and inject it via @Value

--- src/main/java/com/example/demo/api/RequestController.java
+++ src/main/java/com/example/demo/api/RequestController.java
@@ -5,18 +5,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RequestController {
+  @Value("${spring.api.url}")
+  private String apiUrl;
 
   @GetMapping("/")
   public String index() throws IOException {
 
     //request
-    //TODO: remove hardcoding of url, get it from env variable
-    URL url = new URL("http://localhost:8080/");
+    URL url = new URL(apiUrl);
     HttpURLConnection con = (HttpURLConnection) url.openConnection();
     con.setRequestMethod("GET");
 
@@ -25,7 +27,7 @@ public class RequestController {
     BufferedReader in = new BufferedReader(
         new InputStreamReader(con.getInputStream()));
     String inputLine;
-    StringBuffer content = new StringBuffer();
+    StringBuilder content = new StringBuilder();
     while ((inputLine = in.readLine()) != null) {
       content.append(inputLine);
     }
@@ -34,7 +36,7 @@ public class RequestController {
     //close the connection
     con.disconnect();
 
-    return "In app2 reading the request from app1: "+ content.toString();
+    return "In app2 reading the request from app1: "+ content;
 
   }
 
